1ece4625928bd5ade49517e518c30fe3f4237594
hoaidee894 <hoaidee894@gmail.com>
2026-07-21 08:12:18 +0700

    Update 05_indexes.sql

--- Du_AN/sql/05_indexes.sql
+++ Du_AN/sql/05_indexes.sql
@@ -1,1 +1,3 @@
--- SQL Script placeholder
+-- Tạo INDEX cho các cột thường xuyên dùng để tìm kiếm và JOIN/GROUP BY
+CREATE INDEX IF NOT EXISTS idx_apps_app_name ON apps(app_name);
+CREATE INDEX IF NOT EXISTS idx_apps_category ON apps(category);
